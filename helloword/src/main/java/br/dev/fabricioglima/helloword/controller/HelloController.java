1a9e0899513f5d961f06a31dad36e6c8fcae7bd9
Fabricio Guimaraes de Lima <fabricioglimadev@gmail.com>
2025-09-29 22:43:16 -0300

    Aula 2

--- helloword/src/main/java/br/dev/fabricioglima/helloword/controller/HelloController.java
+++ helloword/src/main/java/br/dev/fabricioglima/helloword/controller/HelloController.java
@@ -1,9 +1,6 @@
 package br.dev.fabricioglima.helloword.controller;
 
-import br.dev.fabricioglima.helloword.model.Produto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,20 +11,6 @@ public class HelloController {
     return "Hello World";
   }
 
-  @GetMapping("/produtos")
-  public Produto getPoduto(){
-    Produto p = new Produto();
-    p.setId(12345);
-    p.setNome("Computador");
-    p.setPreco(1500);
-    return p;
-  }
 
-  @PostMapping("produtos")
-  public String addNewProduct(@RequestBody Produto p){
-    System.out.println("Produto recebido");
-    System.out.println(p.getId() +"/"+p.getNome()+"/"+p.getPreco());
-    return "ok";
-  }
 
 }
